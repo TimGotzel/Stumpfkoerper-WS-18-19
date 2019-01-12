c691ed287841a5d77e0328c07299432ecfa22bd2
FlorianTimmBS <f.timm@tu-braunschweig.de>
2019-01-12 16:31:25 +0100

    Werte für C_Power und Abb. für Reine Rotation

--- 40_Messung/Projektarbeit_WS_18_19/Matlab-Auswertung/Druckverteilung und Widerstandsbestimmung/Grafikplotvorlage_Cw_ueber_n.m
+++ 40_Messung/Projektarbeit_WS_18_19/Matlab-Auswertung/Druckverteilung und Widerstandsbestimmung/Grafikplotvorlage_Cw_ueber_n.m
@@ -1,14 +1,20 @@
 n = [0 500 1000 1500 1800 1900 2000 2100 2200 2300 2400 2500];
-x = n;
-y = CwWerteRK([1:12]);
-y_2 = CwWerte([1:12]);
+
+x1 = C_Power_0kPa_RK;
+x2 = C_Power_0kPa;
+for k=1:12
+Delta_Cw_RK(k) = CwWerteRK(k)/CwWerteRK(1);
+Delta_Cw(k) = CwWerte(k)/CwWerte(1);
+end
+y = Delta_Cw_RK([1:12]);
+y_2 = Delta_Cw([1:12]);
 figure()
-plot(x,y,'-x', x, y_2, '-o')
+plot(x1,y, '-', x2, y_2, '-')
 
 %'Color',[1.00,0.41,0.16],'MarkerSize',6,
 
 %title(['Cw:_',num2str(
-ylabel('C_{W} [-]')
-xlabel('n [min^{-1}]')
+ylabel('\Delta C_{W} [-]')
+xlabel('C_{Power,M} [-]')
 %set(gca,'box','on')
 %legend('p_t Daten', 'p Daten', 'p_t Verauf (fit)' , 'p Verlauf (fit)', 'location', 'northeastoutside')
---
+++ 40_Messung/Projektarbeit_WS_18_19/Matlab-Auswertung/Druckverteilung und Widerstandsbestimmung/leistungskoeffizient_m.m
@@ -0,0 +1,6 @@
+function [C_Power_M] = leistungskoeffizient_m(P_M)
+A_ref = 0.0534 * 0.39;
+U_inf=14.94;
+rho= 1.169;
+C_Power_M = P_M/(0.5*rho*U_inf*U_inf*U_inf*A_ref);
+end
