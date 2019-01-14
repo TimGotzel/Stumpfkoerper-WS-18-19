3836874521165117adcb31379ad73134acaaa0ac
FlorianTimmBS <f.timm@tu-braunschweig.de>
2019-01-14 17:41:43 +0100

    Abbildungen für Kapitel 6+ Änderungen an Kap 6

--- 40_Messung/Projektarbeit_WS_18_19/Matlab-Auswertung/Druckverteilung und Widerstandsbestimmung/PRfuer50.m
+++ 40_Messung/Projektarbeit_WS_18_19/Matlab-Auswertung/Druckverteilung und Widerstandsbestimmung/PRfuer50.m
@@ -6,6 +6,7 @@ PR_n = zeros(5,12);
 
 %%Konstanten
 Cw_bez = 0.804692213249081;
+n = [0 500 1000 1500 1800 1900 2000 2100 2200 2300 2400 2500];
 A_ref = 0.39*0.0534;
 A_jet = 0.384*0.0003;
 U_inf = 14.94;
@@ -13,7 +14,8 @@ rho = 1.17;
 h_spalt_o = 0.00019;
 b = 0.384;
 A_jet = 0.384*h_spalt_o;
-p_Plenum = [0 1000 2000 3000 4000];
+p_Plenum0 = [0 1000 2000 3000 4000];
+p_Plenum = [0 1800 4500 5400 6500];
 
 folder = 'C:\Users\Flo\Documents\GitHub\Stumpfkoerper-WS-18-19\40_Messung\Projektarbeit_WS_18_19\Daten\Walzenpaar 50\Aktuation';
 
@@ -27,6 +29,9 @@ for j = 1 :length(list_2) - 2
         PR_n(j,i) = ((rho*((sqrt((2*p_Plenum(j))/rho))^3)*h_spalt_o*b) + P_M(j,i));
         PR(j,i) = (PR_z(j,i)/PR_n(j,i));
         end
+        PR_z(j,1) = ((Cw_bez - Cw50(j,1))*0.5*rho*U_inf*U_inf*U_inf * A_ref);
+        PR_n(j,1) = ((rho*((sqrt((2*p_Plenum0(j))/rho))^3)*h_spalt_o*b) + P_M(j,1));
+        PR(j,1) = (PR_z(j,1)/PR_n(j,1));
      end
 end
 
